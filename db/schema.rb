a90d62aca3db2db601c583b810b0e9b1c02a420c
ctballentine <ctballentine@gmail.com>
2019-12-02 02:10:18 -0500

    finish postgres

--- db/schema.rb
+++ db/schema.rb
@@ -15,54 +15,12 @@ ActiveRecord::Schema.define(version: 20191129191004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "datapoints", id: :serial, force: :cascade do |t|
-    t.integer "event"
-    t.string "parameter", limit: 255
-    t.float "value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "experiment_id"
-  end
-
-  create_table "experiments", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
-    t.string "fcsfile", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "team_id"
-  end
-
-  create_table "keynames", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
-    t.string "value", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "experiment_id"
-  end
-
-  create_table "members", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
-    t.string "interest", limit: 255
-    t.string "location", limit: 255
-    t.string "email", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "team_id"
-  end
-
   create_table "smples", force: :cascade do |t|
     t.float "par1"
     t.float "par2"
     t.string "grp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "teams", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
-    t.string "affiliation", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
 end
